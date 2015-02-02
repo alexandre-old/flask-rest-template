f4ad43b0fcb051f8f85ea582b27ec14cb30e90b0
Alexandre Souza <alexandre_@fastmail.com>
2015-02-01 23:39:07 -0200

    remove example blueprint

--- app/example/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from flask.ext.restful import Api
-from . import routes
-
-
-blueprint = Blueprint('example', __name__)
-api_example = Api(blueprint, prefix='/api')
-
-routes.register_resources(api_example)
