7f12941604b09cf2b94a2df3f98b3d17d235c688
Alexandre Souza <alexandre_@fastmail.com>
2015-02-09 01:08:35 -0200

    created test cases

--- tests/users/unit/test_users.py
+++ tests/users/unit/test_users.py
@@ -1,4 +1,102 @@
 import pytest
 
 
-p = pytest.mark.parametrize
+class TestUserController():
+
+    """Docstring for TestUserController. """
+
+    def test_get_users_without_filter(self, app_db, mock_user):
+        """TODO: Docstring
+
+        :returns: TODO
+
+        """
+        pass
+
+    def test_get_users_by_id(self, app_db, mock_user):
+        """TODO: Docstring for test_get_users_by_id.
+
+        :app_db: TODO
+        :mock_user: TODO
+        :returns: TODO
+
+        """
+        pass
+
+    def test_get_users_by_username(self, app_db, mock_user):
+        """TODO: Docstring for test_get_users_by_username.
+
+        :returns: TODO
+
+        """
+        pass
+
+    def test_get_users_invalid_data(self):
+        """TODO: Docstring for test_get_users_invalid_data.
+
+        :returns: TODO
+
+        """
+        pass
+
+    def test_create_user_invalid_username(self, arg1):
+        """TODO: Docstring for test_create_user_invalid_username.
+
+        :arg1: TODO
+        :returns: TODO
+
+        """
+        pass
+    def test_create_user_valid_username(self, arg1):
+        """TODO: Docstring for test_create_user_valid_username.
+
+        :arg1: TODO
+        :returns: TODO
+
+        """
+        pass
+
+    def test_create_user_too_short_password(self, arg1):
+        """TODO: Docstring for test_create_user_too_short_password.
+
+        :arg1: TODO
+        :returns: TODO
+
+        """
+        pass
+
+    def test_update_user_invalid_username(self, arg1):
+        """TODO: Docstring for test_update_user_invalid_username.
+
+        :arg1: TODO
+        :returns: TODO
+
+        """
+        pass
+
+    def test_update_user_valid_username(self, arg1):
+        """TODO: Docstring for test_update_user_valid_username.
+
+        :arg1: TODO
+        :returns: TODO
+
+        """
+        pass
+
+    def test_delete_user_invalid_id(self, arg1):
+        """TODO: Docstring for test_delete_user_invalid_id.
+
+        :arg1: TODO
+        :returns: TODO
+
+        """
+        pass
+
+    def test_delete_user_valid_id(self, arg1):
+        """TODO: Docstring for test_delete_user_valid_id.
+
+        :arg1: TODO
+        :returns: TODO
+
+        """
+        pass
