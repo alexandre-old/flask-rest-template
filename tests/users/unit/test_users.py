4395a83f94c38ddccd2f098fae71bf01edc21a7a
Alexandre Souza <alexandre_@fastmail.com>
2015-03-02 23:05:55 -0300

    deleted tests with pass

--- tests/users/unit/test_users.py
+++ tests/users/unit/test_users.py
@@ -2,91 +2,10 @@ class TestUserController():
 
     """Docstring for TestUserController. """
 
-    def test_get_users_without_filter(self, app_db, mock_user):
-        """TODO: Docstring
+    def test_get_user_invalid_id(self, app_db):
+        """TODO: Docstring for test_get_user.
 
-        :returns: TODO
-
-        """
-        pass
-
-    def test_get_users_by_id(self, app_db, mock_user):
-        """TODO: Docstring for test_get_users_by_id.
-
-        :app_db: TODO
-        :mock_user: TODO
-        :returns: TODO
-
-        """
-        pass
-
-    def test_get_users_by_username(self, app_db, mock_user):
-        """TODO: Docstring for test_get_users_by_username.
-
-        :returns: TODO
-
-        """
-        pass
-
-    def test_get_users_invalid_data(self):
-        """TODO: Docstring for test_get_users_invalid_data.
-
-        :returns: TODO
-
-        """
-        pass
-
-    def test_create_user_invalid_username(self):
-        """TODO: Docstring for test_create_user_invalid_username.
-
-        :returns: TODO
-
-        """
-        pass
-
-    def test_create_user_valid_username(self):
-        """TODO: Docstring for test_create_user_valid_username.
-
-        :returns: TODO
-
-        """
-        pass
-
-    def test_create_user_too_short_password(self):
-        """TODO: Docstring for test_create_user_too_short_password.
-
-        :returns: TODO
-
-        """
-        pass
-
-    def test_update_user_invalid_username(self):
-        """TODO: Docstring for test_update_user_invalid_username.
-
-        :returns: TODO
-
-        """
-        pass
-
-    def test_update_user_valid_username(self, mock_user):
-        """TODO: Docstring for test_update_user_valid_username.
-
-        :returns: TODO
-
-        """
-        pass
-
-    def test_delete_user_invalid_id(self):
-        """TODO: Docstring for test_delete_user_invalid_id.
-
-        :returns: TODO
-
-        """
-        pass
-
-    def test_delete_user_valid_id(self, mock_user):
-        """TODO: Docstring for test_delete_user_valid_id.
-
+        :id: TODO
         :returns: TODO
 
         """
