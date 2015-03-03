7d415fb60c9645021090c14e6ad8edef79a018b3
Alexandre Souza <alexandre_@fastmail.com>
2015-03-02 22:53:44 -0300

    update fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,10 @@
 from mocks.mock_users import mock_user
+from app import create_app
 import pytest
+import os
 
 
-@pytest.fixture
+@pytest.fixture(scope='module')
 def app_db(request):
     """TODO: Docstring for app_db.
 
@@ -10,4 +12,18 @@ def app_db(request):
     :returns: TODO
 
     """
-    pass
+    app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+    ctx = app.app_context()
+
+    ctx.push()
+
+    def tear_down():
+        """TODO: Docstring for tear_down.
+        :returns: TODO
+
+        """
+        ctx.pop()
+
+    request.addfinalizer(tear_down)
+
+    return app
