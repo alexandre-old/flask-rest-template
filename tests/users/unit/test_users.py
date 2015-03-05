e77b82ddd416488d019a848935dc0d4d5598892b
Alexandre Souza <alexandre_@fastmail.com>
2015-03-05 02:54:21 -0300

    updated tests

--- tests/users/unit/test_users.py
+++ tests/users/unit/test_users.py
@@ -1,12 +1,40 @@
-class TestUserController():
+from app.users import controllers
 
-    """Docstring for TestUserController. """
 
-    def test_get_user_invalid_id(self, app_db):
-        """TODO: Docstring for test_get_user.
+def test_get_user_invalid_filter(app_db):
+    """TODO: Docstring for .
 
-        :id: TODO
-        :returns: TODO
+    :id: TODO
+    :returns: TODO
 
-        """
-        pass
+    """
+    controller = controllers.UserController()
+    result = controller.get_user({'invalid_attr': 'invalid value'}).keys()
+
+    assert 'error' in result
+
+
+def test_get_user_valid_username_with_mock(app_db, mock_user):
+    """TODO: Docstring for test_get_user_valid_username.
+
+    :arg1: TODO
+    :returns: TODO
+
+    """
+    controller = controllers.UserController()
+    result = controller.get_user({'username': 'mockuser'}).keys()
+
+    assert 'success' in result
+
+
+def test_get_user_valid_username_without_mock(app_db):
+    """TODO: Docstring for test_get_user_valid_username_without_mock.
+
+    :app_db: TODO
+    :returns: TODO
+
+    """
+    controller = controllers.UserController()
+    result = controller.get_user({'username': 'mockuser'}).keys()
+
+    assert 'no-data' in result
