959e536ca9c0c4262b0e4df472968bd2f4687fbe
Alexandre Souza <alexandre_@fastmail.com>
2015-04-14 22:28:14 -0300

    restart

--- app/users/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from flask.ext.restful import Api
-from . import routes, models
-
-
-blueprint = Blueprint('users', __name__)
-api_users = Api(blueprint, prefix='/api')
-
-routes.register_resources(api_users)
