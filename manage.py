fd3e7e345276f373c42729f8c9a1478758c7986b
Alexandre Souza <alexandre_@fastmail.com>
2015-04-18 16:53:18 -0300

    initial manage.py

---
+++ manage.py
@@ -0,0 +1,20 @@
+import os
+from flask.ext.script import Manager, Shell, Server
+from .app import create_app
+from .app.extensions import db
+
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+manager = Manager(app)
+
+# access python shell with context
+manager.add_command(
+    "shell",
+    Shell(make_context=lambda: {'app': app, 'db': db}), use_ipython=True)
+
+# run the app
+manager.add_command(
+    "startserver",
+    Server(port=(os.getenv('FLASK_PORT') or 5000), host='0.0.0.0'))
+
