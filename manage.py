33e414c32924a9b30e92705588ec01ac1b13d372
Alexandre Souza <alexandre_@fastmail.com>
2015-04-18 18:11:52 -0300

    remove circular import

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 import os
 from flask.ext.script import Manager, Shell, Server
-from .app import create_app
-from .app.extensions import db
+from app import create_app
+from app.extensions import db
 
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
