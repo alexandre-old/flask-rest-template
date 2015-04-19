3fb544555a18b41a729c1fd1842d381cf4327265
Alexandre Souza <alexandre_@fastmail.com>
2015-04-19 14:35:09 -0300

    added initial resources

---
+++ app/users/controllers.py
@@ -0,0 +1,39 @@
+def is_an_available_username(username):
+    """Verify if an username is available
+
+    :username: a string object
+    :returns: True or False
+
+    """
+    pass
+
+
+def get_users(username=None):
+    """Get all users info. Accepts specify an username.
+
+    :username: a string object
+    :returns: a dict with the operation result
+
+    """
+    pass
+
+
+def create_or_update_user(username, password):
+    """Creates or updates an user.
+
+    :username: a string object
+    :password: a string object (plaintext)
+    :returns: a dict with the operation result
+
+    """
+    pass
+
+
+def disable_user(username):
+    """Disable an user.
+
+    :username: a string object
+    :returns: a dict with the operation result
+
+    """
+    pass
