9aafa20da28a4afd178da91a25e1e86015f26cc1
Alexandre Souza <alexandre_@fastmail.com>
2015-04-22 09:44:11 -0300

    define route

--- app/users/__init__.py
+++ app/users/__init__.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
 from flask.ext.restful import Api
+from . import resources
 
 
 blueprint = Blueprint('users', __name__)
-api = Api(prefix='/api')
-api.init_app(blueprint)
+api = Api(blueprint, prefix='/api')
+
+api.add_resource(resources.UsersAPI, '/users')
