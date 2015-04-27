9bc73f43437b0e1ccac2ac674291d44609322f4e
Alexandre Souza <alexandre_@fastmail.com>
2015-04-27 03:08:17 -0300

    added a function to generate a standard api response

--- app/helpers.py
+++ app/helpers.py
@@ -1,4 +1,6 @@
 import collections
+import functools
+import json
 import flask
 from passlib import hash
 from flask.ext.restful import Api
@@ -70,13 +72,54 @@ def verify_password(password, hash):
     return method.verify(password, hash)
 
 
-def standardize_api_response(result):
-    """Creates a standardized response. This function eval the result key
-    to define the status code and data.
+def standardize_api_response(function):
+    """ Creates a standardized response. This function should be used as a deco
+    rator.
+    :function: The function decorated should return a dict with one of
+    the keys  bellow:
+        success -> GET, 200
+        error -> Bad Request, 400
+        created -> POST, 201
+        updated -> PUT, 200
+        no-data -> No Content, 204
 
-    :result: a dict object with the operation result and on of the keys:
-        'success', 'error', 'created', 'updated' or 'no-data'.
-    :returns: TODO
-
+    :returns: json.dumps(response), staus code
     """
-    pass
+
+    available_result_keys = [
+        'success', 'error', 'created', 'updated', 'no-data']
+
+    status_code_and_descriptions = {
+        'success': (200, 'Successful Operation'),
+        'error': (400, 'Bad Request'),
+        'created': (201, 'Successfully created'),
+        'updated': (200, 'Successfully updated'),
+        'no-data': (204, '')
+    }
+
+    @functools.wraps(function)
+    def make_response(*args, **kwargs):
+
+        result = function(*args, **kwargs)
+
+        if not set(available_result_keys) & set(result):
+            raise ValueError('Invalid result key.')
+
+        status_code, description = status_code_and_descriptions[
+            next(iter(result.keys()))
+        ]
+
+        status_code = ('status_code', status_code)
+        description = (
+            ('description', description) if status_code[1] != 400 else
+            ('error', description)
+        )
+        data = (
+            ('data', next(iter(result.values()))) if status_code[1] != 204 else
+            ('data', '')
+        )
+
+        return json.dumps(collections.OrderedDict([
+            status_code, description, data])), status_code[-1]
+
+    return make_response
